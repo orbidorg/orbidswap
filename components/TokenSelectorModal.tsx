86ed35c94d12837fba9238370be1a9da99298359
orbidorg <orbidorg@gmail.com>
2025-12-06 15:45:02 -0500

    fix: remove USDT/ETH and restore TokenSelectorModal

--- components/TokenSelectorModal.tsx
+++ components/TokenSelectorModal.tsx
@@ -16,7 +16,6 @@ const COMMON_TOKENS = [
     { symbol: 'WLD', name: 'Worldcoin', address: '0x2cFc85d8E48F8EAB294be644d9E25C3030863003' },
     { symbol: 'WETH', name: 'Wrapped Ether', address: '0xdBd74deF5339C659719Afd3f533412b5de4D3736' },
     { symbol: 'USDC', name: 'USD Coin', address: '0x79A02482A880bCE3F13e09Da970dC34db4CD24d1' },
-    { symbol: 'USDT', name: 'Tether USD', address: '0x...' },
 ]
 
 export function TokenSelectorModal({ isOpen, onClose, onSelect, excludeToken }: TokenSelectorModalProps) {
