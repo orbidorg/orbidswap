e550ffb1f0053eec7dcb407823f0f4e69846efb6
orbidorg <orbidorg@gmail.com>
2025-12-06 15:50:44 -0500

    feat: add WBTC and sDAI to token selector

--- components/TokenSelectorModal.tsx
+++ components/TokenSelectorModal.tsx
@@ -17,6 +17,8 @@ const COMMON_TOKENS = [
     { symbol: 'WLD', name: 'Worldcoin', address: '0x2cFc85d8E48F8EAB294be644d9E25C3030863003' },
     { symbol: 'WETH', name: 'Wrapped Ether', address: '0xdBd74deF5339C659719Afd3f533412b5de4D3736' },
     { symbol: 'USDC', name: 'USD Coin', address: '0x79A02482A880bCE3F13e09Da970dC34db4CD24d1' },
+    { symbol: 'WBTC', name: 'Wrapped Bitcoin', address: '0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3' },
+    { symbol: 'sDAI', name: 'Savings Dai', address: '0x859dbe24b90c9f2f7742083d3cf59ca41f55be5d' },
 ]
 
 export function TokenSelectorModal({ isOpen, onClose, onSelect, excludeToken }: TokenSelectorModalProps) {
